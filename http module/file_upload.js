ea1a9edc53900e1665fe20273ba8862631a36a85
amit <amitkumar1211@gmail.com>
2021-08-27 15:12:09 +0530

    Signed-off-by: amit <amitkumar1211@gmail.com>

--- http module/file_upload.js
+++ http module/file_upload.js
@@ -1,40 +1,25 @@
 var http = require('http');
+var formidable = require('formidable');
 var fs = require('fs');
-//to parse html form data.
-var formidable = require('formidable'); 
-
-// html file containing upload form
-var upload_html = fs.readFileSync("upload_file.html");
-
-// replace this with the location to save uploaded files
-var upload_path = "upload_file/";
 
 http.createServer(function (req, res) {
-    if (req.url == '/uploadform') {
-		
-      res.writeHead(200);
-      res.write(upload_html);
-      return res.end();
-	  
-    } else if (req.url == '/fileupload') {
-		
-        var form = new formidable.IncomingForm();
-        console.log("file received")
-		
-        form.parse(req, function (err, fields, files) {
-			
-            // oldpath : temporary folder to which file is saved to
-            var oldpath = files.filetoupload.path;
-            var newpath = upload_path + files.filetoupload.name;
-			
-            // copy the file to a new location
-            fs.rename(oldpath, newpath, function (err) {
-                if (err) throw err;
-				
-                // you may respond with another html page
-                res.write('File uploaded and moved!');
-                res.end();
-            });
-        });
-    } 
-}).listen(5000);
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = './' + files.filetoupload.name;
+      fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+        res.write('File uploaded and moved!');
+        res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
